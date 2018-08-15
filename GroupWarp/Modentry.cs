489c82ee3f558155a3159cb4203e178bcd573bbc
phase858 <phase858@gmail.com>
2018-08-14 23:41:11 -0400

    words

--- GroupWarp/Modentry.cs
+++ GroupWarp/Modentry.cs
@@ -26,19 +26,20 @@ namespace GroupWarp
 
                 foreach (Farmer farmer in Game1.getOnlineFarmers())
                 {
-                    if (farmer != Game1.player)
+                    if (farmer.Name != Game1.player.Name)
                     {
-                        this.Monitor.Log(farmer.Name);
+                        /*this.Monitor.Log(farmer.Name);
                         this.Monitor.Log(power.ToString());
                         this.Monitor.Log(farmer.currentLocation.ToString());
-                        Game1.player.CurrentTool.DoFunction(farmer.currentLocation, farmer.getStandingX(), farmer.getStandingY(), 45, farmer);
+                        Game1.player.CurrentTool.DoFunction(farmer.currentLocation, farmer.getStandingX(), farmer.getStandingY(), 45, farmer);*/
+                        farmer.warpFarmer(new Warp (farmer.getStandingX(), farmer.getStandingY(), "Farm", 64, 15, false));
                     }                
                 }
                 //Game1.player.CurrentTool.DoFunction(Game1.player.currentLocation, Game1.player.getStandingX(), Game1.player.getStandingY(), 45, Game1.player);
 
                 //Game1.warpFarmer("Farm", 64, 15, false);
                 //int power = (int)(((double)Game1.toolHold + 20.0) / 600.0) + 1;
-                Game1.player.CurrentTool.DoFunction(Game1.currentLocation, Game1.player.getStandingX(), Game1.player.getStandingY(), power, Game1.player);
+                //Game1.player.CurrentTool.DoFunction(Game1.currentLocation, Game1.player.getStandingX(), Game1.player.getStandingY(), power, Game1.player);
             }
         }
     }
